54d0c5a9e571d9177ca92ed5fdba1b5139faa525
Nikolas Krecu <nkrecu2211@jh-mba158-07.local>
2019-02-13 11:10:19 -0600

    added index path

---
+++ Shopping List/ITEM.swift
@@ -0,0 +1,17 @@
+//
+//  ITEM.swift
+//  Shopping List
+//
+//  Created by Nikolas Krecu on 2/12/19.
+//  Copyright © 2019 Nikolas Krecu. All rights reserved.
+//
+
+import Foundation
+class Item{
+var name: String
+    init(name:String){
+    self.name = name
+
+
+}
+}
--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource{
+    @IBOutlet weak var newItemTextfield: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    var Items: [Item] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+        let item1 = Item(name: "Milk")
+        let item2 = Item(name: "eggs")
+        Items = [item1, item2]
+        let item3 = Item(name: "Bread")
+        Items.append(item3)
     }
 
-
+    @IBAction func addNewItemButtonPressed(_ sender: Any) {
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return Items.count
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        let currentItemName = Items[indexPath.row].name
+        cell.textLabel?.text = "Hello"
+        cell.textLabel?.text = currentItemName
+        return cell
+    }
+    
+    
 }
 
